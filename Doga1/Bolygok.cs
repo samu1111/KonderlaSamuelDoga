fcf5077d3b2fc2600f7d976ce75c28de482c4eca
Sámuel Konderla <samuelkonderla@gmail.com>
2023-10-25 09:47:00 +0200

    Add project files.

---
+++ Doga1/Bolygok.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doga1
+{
+    internal class Bolygok
+    {
+
+        public string Nev { get; set; }
+        public int Holdak { get; set; }
+        public double Arany { get; set; }
+
+
+        public Bolygok(string sor) {
+            string[] adat = sor.Split(';');
+            Nev  = adat[0];
+            Holdak = int.Parse(adat[1]);
+            Arany = double.Parse(adat[2].Replace('.', ','));
+        
+        
+        
+        }
+
+    }
+}
---
+++ Doga1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using System.Text;
+
+namespace Doga1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Bolygok> lista = new List<Bolygok>();
+            using (StreamReader sr = new StreamReader(path: @"..\..\..\Mappa\solsys.txt", encoding: Encoding.UTF8))
+            {
+                while (!sr.EndOfStream)
+                {
+                    lista.Add(new Bolygok(sr.ReadLine()));
+                }
+            }
+            Console.WriteLine("3. feladat:");
+            Console.WriteLine($"\t3.1: {lista.Count} bolygó van a naprendszerben");
+            var atlag = lista.Average(b => b.Holdak);
+            Console.WriteLine($"3.2: A naprendszerben egy bolygónak átlagosan {atlag} holdja van");
+            var max = lista.SingleOrDefault(b => b.Arany == lista.Max(c => c.Arany));
+            Console.WriteLine($"3.3: A legnagyobb térfogatú bolygó a " + 
+                $"{max.Nev}");
+            Console.Write($"3.4: Írd be a keresett Bolygó nevét: ");
+            var ize = Console.ReadLine();
+            Console.WriteLine(lista.SingleOrDefault(b => b.Nev == ize) != null ?
+                "Van ilyen bolygó a naprendszerben." :
+                "Sajnos nincs ilyen bolygó a naprendszerben. ");
+
+            Console.WriteLine("3.5: Írj be egy egész számot: ");
+            var szam = int.Parse(Console.ReadLine());
+            var hold = lista.Where(b => b.Holdak > szam).Select(b => b.Nev).ToArray();
+            Console.WriteLine($"A következő bolygóknak van {szam}-nál/nél több holdja ");
+            foreach (var item in hold)
+            {
+                Console.Write($"{item}, ");
+            }  
+            
+
+
+
+        }
+    }
+}
